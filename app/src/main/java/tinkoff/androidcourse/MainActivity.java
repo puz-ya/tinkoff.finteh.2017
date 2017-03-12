a95406bb07f31181748f236692d90d66e8afdf7e
winstonvk <vkokhanov93@gmail.com>
2017-03-12 22:15:23 +0300

    lesson2: add build variants + flavours, add gson with proguard, add signing configs

--- app/src/main/java/tinkoff/androidcourse/MainActivity.java
+++ app/src/main/java/tinkoff/androidcourse/MainActivity.java
@@ -2,6 +2,10 @@ package tinkoff.androidcourse;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Gson gson = new GsonBuilder().create();
+        Response response = gson.fromJson("{ value:\"bla bla bla\" }", Response.class);
+        Toast.makeText(this, "Response: " + response.value, Toast.LENGTH_LONG).show();
     }
 }
---
+++ app/src/main/java/tinkoff/androidcourse/Response.java
@@ -0,0 +1,13 @@
+package tinkoff.androidcourse;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * @author Vladimir Kokhanov
+ */
+
+public class Response {
+
+    @SerializedName("value")
+    String value;
+}
