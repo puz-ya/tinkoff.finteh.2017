eeff06995150f0708f062815c91f5450907f12f2
puz_ya <puzino.y.a@gmail.com>
2017-03-15 22:41:28 +0300

    Tested picasso plugin - success; Deleted old test files.

--- app/src/main/java/tinkoff/androidcourse/MainActivity.java
+++ app/src/main/java/tinkoff/androidcourse/MainActivity.java
@@ -2,10 +2,12 @@ package tinkoff.androidcourse;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,5 +19,10 @@ public class MainActivity extends AppCompatActivity {
         Gson gson = new GsonBuilder().create();
         Response response = gson.fromJson("{ value:\"bla bla bla\" }", Response.class);
         Toast.makeText(this, "Response: " + response.value, Toast.LENGTH_LONG).show();
+
+        /*
+        ImageView imageView = (ImageView) findViewById(R.id.image_test);
+        Picasso.with(this).load("http://i.imgur.com/DvpvklR.png").into(imageView);
+        //*/
     }
 }
