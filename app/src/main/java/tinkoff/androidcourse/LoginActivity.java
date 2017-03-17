32717e85a90b8ded0aa28aa0d147812e090288c8
puz_ya <puzino.y.a@gmail.com>
2017-03-17 21:28:37 +0300

    Lesson03: Simple Login Activity (2 EditText). Nothing to code.

---
+++ app/src/main/java/tinkoff/androidcourse/LoginActivity.java
@@ -0,0 +1,29 @@
+package tinkoff.androidcourse;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText login;
+    private EditText password;
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        login = (EditText) findViewById(R.id.edit_text_login);
+        password = (EditText) findViewById(R.id.edit_text_password);
+    }
+
+}
