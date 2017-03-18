6d8a22da692a42632c729dbf2fdd7c8942d187b7
puz_ya <puzino.y.a@gmail.com>
2017-03-18 23:39:40 +0300

    Fix merge conflicts.

--- app/src/main/java/tinkoff/androidcourse/LoginActivity.java
+++ app/src/main/java/tinkoff/androidcourse/LoginActivity.java
@@ -1,10 +1,8 @@
 package tinkoff.androidcourse;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,13 +15,26 @@ public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         login = (EditText) findViewById(R.id.edit_text_login);
         password = (EditText) findViewById(R.id.edit_text_password);
+
+        button = (Button) findViewById(R.id.btn_enter);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                startNextScreen();
+            }
+        });
     }
 
+    private void startNextScreen() {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("LOGIN", login.getText().toString());
+        startActivity(intent);
+    }
 }
