49bf2d65a492217136f05ff02790dbf5a937ad44
winstonvk <vkokhanov93@gmail.com>
2017-04-02 00:50:54 +0300

    lesson5: add ProgressButton & vector drawable

--- app/src/main/java/tinkoff/androidcourse/LoginActivity.java
+++ app/src/main/java/tinkoff/androidcourse/LoginActivity.java
@@ -1,17 +1,21 @@
 package tinkoff.androidcourse;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
+
+import tinkoff.androidcourse.ui.widgets.ProgressButton;
 
 public class LoginActivity extends AppCompatActivity {
 
     private EditText login;
     private EditText password;
-    private Button button;
+    private ProgressButton button;
+
+    private LoginTask task = new LoginTask();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +24,12 @@ public class LoginActivity extends AppCompatActivity {
 
         login = (EditText) findViewById(R.id.edit_text_login);
         password = (EditText) findViewById(R.id.edit_text_password);
-        button = (Button) findViewById(R.id.btn_enter);
+        button = (ProgressButton) findViewById(R.id.btn_enter);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startNextScreen();
+                task.execute();
             }
         });
     }
@@ -34,6 +38,30 @@ public class LoginActivity extends AppCompatActivity {
         Intent intent = new Intent(this, MainActivity.class);
         intent.putExtra("LOGIN", login.getText().toString());
         startActivity(intent);
+    }
+
+    private class LoginTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            button.showProgress();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            button.hideProgress();
+            startNextScreen();
+        }
     }
 }
 
