7fd9487daf86851e9cf4d5af2630411ca8dffb49
puz-ya <puzino.y.a@gmail.com>
2017-05-21 17:41:38 +0300

    Step 3 - added unique id to views of CustomProgressButton, added onSaveInstanceState to ProgressButton.

--- app/src/main/java/tinkoff/androidcourse/ui/widgets/ProgressButton.java
+++ app/src/main/java/tinkoff/androidcourse/ui/widgets/ProgressButton.java
@@ -4,6 +4,8 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.PorterDuff;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
@@ -37,9 +39,9 @@ public class ProgressButton extends FrameLayout implements View.OnTouchListener 
 
     private void init(AttributeSet attrs) {
         LayoutInflater.from(getContext()).inflate(R.layout.widget_progress_button, this);
-        background = findViewById(R.id.view_background);
-        textView = (TextView) findViewById(R.id.text_view);
-        progressBar = (ProgressBar) findViewById(R.id.progress_bar);
+        background = findViewById(R.id.custom_PB_background);
+        textView = (TextView) findViewById(R.id.custom_PB_textView);
+        progressBar = (ProgressBar) findViewById(R.id.custom_PB_progressbar);
         progressBar.getIndeterminateDrawable().setColorFilter(ContextCompat.getColor(getContext(), R.color.grey), PorterDuff.Mode.SRC_ATOP);
 
         final TypedArray typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.ProgressButton);
@@ -87,5 +89,66 @@ public class ProgressButton extends FrameLayout implements View.OnTouchListener 
     HOWTO set & get CustomView state
     http://stackoverflow.com/questions/3542333/how-to-prevent-custom-views-from-losing-state-across-screen-orientation-changes
      */
+    @Override
+    public Parcelable onSaveInstanceState(){
+        //begin boilerplate code that allows parent classes to save state
+        Parcelable superState = super.onSaveInstanceState();
+        SavedState savedState = new SavedState(superState);
 
+        savedState.clickable = isClickable();
+        savedState.textViewVisibility = textView.getVisibility();
+        savedState.progressBarVisibility = progressBar.getVisibility();
+        return savedState;
+    }
+
+    @Override
+    public void onRestoreInstanceState(Parcelable state){
+        if(!(state instanceof SavedState)) {
+            super.onRestoreInstanceState(state);
+            return;
+        }
+        SavedState savedState = (SavedState) state;
+        super.onRestoreInstanceState(savedState.getSuperState());
+
+        setClickable(savedState.clickable);
+        textView.setVisibility(savedState.textViewVisibility);
+        progressBar.setVisibility(savedState.progressBarVisibility);
+    }
+
+    static class SavedState extends BaseSavedState{
+        boolean clickable;
+        int textViewVisibility;
+        int progressBarVisibility;
+
+        public SavedState(Parcelable source) {
+            super(source);
+        }
+
+        private SavedState(Parcel in) {
+            super(in);
+            this.clickable = (in.readInt() == 1);
+            this.textViewVisibility = in.readInt();
+            this.progressBarVisibility = in.readInt();
+        }
+
+        @Override
+        public void writeToParcel(Parcel out, int flags){
+            super.writeToParcel(out, flags);
+            out.writeInt(clickable ? 1 : 0);
+            out.writeInt(textViewVisibility);
+            out.writeInt(progressBarVisibility);
+        }
+
+        //required field that makes Parcelables from a Parcel
+        public static final Parcelable.Creator<SavedState> CREATOR =
+                new Parcelable.Creator<SavedState>() {
+                    public SavedState createFromParcel(Parcel in) {
+                        return new SavedState(in);
+                    }
+                    public SavedState[] newArray(int size) {
+                        return new SavedState[size];
+                    }
+                };
+
+    }
 }
